739f620eb9e585bf29d7e118ab64350da7981ba5
seshimo8 <75005267+seshimo8@users.noreply.github.com>
2020-12-29 21:44:04 +0900

    新規登録機能を実装する（バリデーション、エラーメッセージの設定）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: "全角カタカナのみで入力して下さい" } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
 end
