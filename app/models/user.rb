b4bc06b9d27c2300014881dd6bbd3df8fe3a8f59
seshimo8 <75005267+seshimo8@users.noreply.github.com>
2021-01-04 15:00:40 +0900

    itemsテーブルとusersテーブルのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
