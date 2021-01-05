522a64c65d840bb769f3f1c2d9baf7ecffb7be51
seshimo8 <75005267+seshimo8@users.noreply.github.com>
2021-01-05 21:37:59 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  
+
 
   def index
   end
@@ -8,13 +10,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def message_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:name, :text, :image, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :day_to_ship_id, :price).merge(user_id: current_user.id)
   end
-
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,13 +5,29 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :Shipping_charge
+  belongs_to :shipping_charge
   belongs_to :prefecture
   belongs_to :day_to_ship
 
-  validates :title, :text, presence: true
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :day_to_ship_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 
-  validates :genre_id, numericality: { other_than: 1 } 
+  validates :price,numericality: { only_integer: true,greater_than: 299, less_than: 10000000}
 
+  with_options presence: true, format: { with: /\A[-]?[0-9]+(\.[0-9]+)?\z/ ,price: "半角数字で入力" } do
+    validates :price
+  end
 
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :shipping_charge_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :day_to_ship_id, numericality: { other_than: 1 } 
 end
