5af2c88fd38ea15e3eb69ee7af8ad15ca541aaa6
seshimo8 <75005267+seshimo8@users.noreply.github.com>
2021-01-05 12:12:53 +0900

    ActiveHashの導入とモデルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,12 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
 
   private
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :Shipping_charge
+  belongs_to :prefecture
+  belongs_to :day_to_ship
+
+  validates :title, :text, presence: true
+
+  validates :genre_id, numericality: { other_than: 1 } 
+
+
 end
