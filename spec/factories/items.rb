a10ed1832b1674be97942a5bde8d9f3848e5cfd6
seshimo8 <75005267+seshimo8@users.noreply.github.com>
2021-01-06 16:16:50 +0900

    ダミーデータ作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,7 +3,13 @@ FactoryBot.define do
     name              { 'test' }
     text              { 'test' }
     price             { '1000' }
-  
+    category_id       {'2'}
+    condition_id      {'2'}
+    shipping_charge_id{'2'}
+    prefecture_id     {'2'}
+    day_to_ship_id    {'2'}
+
+
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
