52d4f79fa6198dd2db1c10eeb1e5a9d1372d89cd
seshimo8 <75005267+seshimo8@users.noreply.github.com>
2021-01-07 18:43:38 +0900

    ルーティングにupdateを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "items#index"
-  resources :items, only: [:new, :create, :show, :edit]
+  resources :items, only: [:new, :create, :show, :edit, :update]
   devise_for :users
 end
