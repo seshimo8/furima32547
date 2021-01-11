2c688626f0d8b4588168bb8aadc85493bc1c64b3
seshimo8 <75005267+seshimo8@users.noreply.github.com>
2021-01-11 19:10:33 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,10 @@ class ItemsController < ApplicationController
     if current_user.id != @item.user_id
       redirect_to root_path
     end
+
+    if current_user.id == @item.user_id && @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def update
